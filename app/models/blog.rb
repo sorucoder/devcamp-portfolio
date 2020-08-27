74648a20b1f4aa36909f0befd1cdfb2d0fbd343c
sorucoder <sorucoder@gmail.com>
2020-08-27 17:54:15 -0400

    Fixed tabbing mistake

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,7 +4,7 @@ class Blog < ApplicationRecord
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body
+	validates_presence_of :title, :body
 
-  belongs_to :topic
+	belongs_to :topic
 end
