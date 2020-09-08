9c32dad8c58a5ae5d93b1577f4a4f0e260fe1170
sorucoder <sorucoder@gmail.com>
2020-09-08 12:00:37 -0400

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular', to: 'portfolios#angular_items'
   get 'rails', to: 'portfolios#rails_items'
