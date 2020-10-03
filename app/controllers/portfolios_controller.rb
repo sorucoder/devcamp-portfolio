40d88803d30c5e38cc36e3e784a003cc661cd70f
sorucoder <sorucoder@gmail.com>
2020-10-03 18:20:09 -0400

    Refactored before action for portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
 
   def index
@@ -19,8 +20,6 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(portfolio_params)
-
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.'}
@@ -31,46 +30,43 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was removed.' }
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(
+        :title,
+        :subtitle,
+        :body,
+        technologies_attributes: [:name]
+    )
+  end
+
+  def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
-
-      respond_to do |format|
-        if @portfolio_item.update(portfolio_params)
-          format.html { redirect_to portfolios_path, notice: 'The portfolio item was successfully updated.' }
-        else
-          format.html { render :edit }
-        end
-      end
-    end
-
-    def show
-      @portfolio_item = Portfolio.find(params[:id])
-    end
-
-    def destroy
-      # Perfom the lookup
-      @portfolio_item = Portfolio.find(params[:id])
-
-      # Destroy/delete the record
-      @portfolio_item.destroy
-
-      # Redirect
-      respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: 'Portfolio item was removed.' }
-      end
-    end
-
-    private
-
-    def portfolio_params
-      params.require(:portfolio).permit(
-          :title,
-          :subtitle,
-          :body,
-          technologies_attributes: [:name]
-      )
-    end
+  end
 end
