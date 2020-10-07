9d0b24fe706dcc756a9fd60a261bcbc5ec899a93
sorucoder <sorucoder@gmail.com>
2020-10-07 11:55:20 -0400

    Implemented bootstrap gem and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
 
