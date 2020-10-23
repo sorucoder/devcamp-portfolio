c281f863238413e7d7c9d7d62e4bff8eaf390389
sorucoder <sorucoder@gmail.com>
2020-10-23 01:24:17 -0400

    Fixed security flaw where user can see unpublished blog via url

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,10 +17,14 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    @page_title = "#{@blog.title} | Marcus Germano, IV"
-    @seo_keywords = @blog.body
+    if logged_in?(:site_admin) || @blog.published?   
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      @page_title = "#{@blog.title} | Marcus Germano, IV"
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
+    end
   end
 
   # GET /blogs/new
@@ -41,11 +45,9 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        redirect_to @blog, notice: 'Blog was successfully created.'
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
