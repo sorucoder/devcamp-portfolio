5c8a3ff3e82910b99dc983ae6409d7ad08cce49d
sorucoder <sorucoder@gmail.com>
2020-10-31 20:31:31 -0400

    Setup ActionCable for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://sorucoder-portfolio.herokuapp.com', 'http://sorucoder-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://sorucoder-portfolio.herokuapp.com/cable"
 end
